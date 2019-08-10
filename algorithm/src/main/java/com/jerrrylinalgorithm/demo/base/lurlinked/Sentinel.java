393cdcf5f01a40f285ece23d57e0a16b2066ddd9
linsnail <linsnail@outlook.com>
2019-08-10 23:37:53 +0800

    add sentinel

--- algorithm/src/main/java/com/jerrrylinalgorithm/demo/base/lurlinked/Sentinel.java
+++ algorithm/src/main/java/com/jerrrylinalgorithm/demo/base/lurlinked/Sentinel.java
@@ -1,7 +1,7 @@
 package main.java.com.jerrrylinalgorithm.demo.base.lurlinked;
 
 /**
- * 哨兵节点简化
+ * 哨兵节点思想：小概率需要的判断先提前扼杀，比如为给值避免为null
  * @author
  */
 public class Sentinel {
@@ -25,36 +25,50 @@ public class Sentinel {
 
     /**
      *在数组a中，查找key,返回key所在位置
-     *
+     *其中，
+    n 表示数组
+    a 的长度
+     * 我举 2 个例子，你可以拿例子走一下代码
+             * // a = {4, 2, 3, 5, 9, 6}  n=6 key = 7
+             * // a = {4, 2, 3, 5, 9, 6}  n=6 key = 6\
+     * 总结:跟上述方法比,就是找到最后数组的位置把要查找的值插进去，从而可以使用if(a(n-1)==key)方法来遍历，省去了i<n的判
+     *      断，但是可读性差，一般不这么写
      */
-    int findBySentinel(char[]a,int n,char key){
-        if(a==null||n<=0){
+
+
+    int findBySentinel(char[] a, int n, char key) {
+        if(a == null || n <= 0) {
             return -1;
         }
-        //这里因为要将a[n-1]的值替换成key,所以要特殊处理这个值
-        if(a[n-1]==key){
+
+        // 这里因为要将 a[n-1] 的值替换成 key，所以要特殊处理这个值
+        if (a[n-1] == key) {
             return n-1;
         }
 
-        //把a[n-1]的值临时保存在变量tmp,以方便回复
-        //之所以这样做是为了：希望find()代码不要改变a数组中的内容
-        char tmp =a[n-1];
-        //把key的值放到a[n-1]中，此时
+        // 把 a[n-1] 的值临时保存在变量 tmp 中，以便之后恢复。tmp=6。
+        // 之所以这样做的目的是：希望 find() 代码不要改变 a 数组中的内容
+        char tmp = a[n-1];
+        // 把 key 的值放到 a[n-1] 中，此时 a = {4, 2, 3, 5, 9, 7}
         a[n-1] = key;
 
-        int i=0;
-        //
-        while(a[i]!=key){
+        int i = 0;
+        // while 循环比起代码一，少了 i<n 这个比较操作
+        while (a[i] != key) {
             ++i;
         }
 
+        // 恢复 a[n-1] 原来的值, 此时 a= {4, 2, 3, 5, 9, 6}
         a[n-1] = tmp;
-        if(i==n-1){
+
+        if (i == n-1) {
+            // 如果 i == n-1 说明，在 0...n-2 之间都没有 key，所以返回 -1
             return -1;
-        }else{
+        } else {
+            // 否则，返回 i，就是等于 key 值的元素的下标
             return i;
         }
-
     }
+
 
 }
